3be7b7d03b231f5676bcb006ba49e3d09a6c2967
Misranfuadi <misranfalcon@gmail.com>
2019-05-30 15:03:15 +0700

    add table responsive

--- resources/views/pages/siswa/index.blade.php
+++ resources/views/pages/siswa/index.blade.php
@@ -6,40 +6,42 @@
             <h4 class="card-header bg-dark text-light mb-3">Siswa</h4>
             <div class="card-body">
                 @if (!empty($siswa_list))
-                    <table class="table">
-                        <thead class="table-primary">
-                          <tr>
-                            <th scope="col">NISN</th>
-                            <th scope="col">Nama</th>
-                            <th scope="col">Tgl Lahir</th>
-                            <th scope="col">Gender</th>
-                            <th scope="col"><a href="siswa/create" class="btn btn-primary btn-sm">Tambah</a></th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          @foreach ($siswa_list as $siswa)
-                            <tr>
-                                <td>{{ $siswa->nisn }}</td>
-                                <td>{{ $siswa->nama_siswa }}</td>
-                                <td>{{ $siswa->tanggal_lahir->format('d-m-Y') }}</td>
-                                <td>{{ $siswa->jenis_kelamin }}</td>
-                                <td>
-                                    <div class="box-button">
-                                        {{ link_to('siswa/'.$siswa->id,'Detail',['class'=>'btn btn-info btn-sm']) }}
-                                    </div>
-                                    <div class="box-button">
-                                        {{ link_to('siswa/'.$siswa->id.'/edit','Edit',['class'=>'btn btn-warning btn-sm']) }}
-                                    </div>
-                                    <div class="box-button">
-                                        {!! Form::open(['method'=> 'Delete','action'=>['SiswaController@destroy',$siswa->id]]) !!}
-                                        {!! Form::submit('Delete',['class'=> 'btn btn-danger btn-sm']) !!}
-                                        {!! Form::close() !!}
-                                    </div>
-                                </td>
-                            </tr>
-                          @endforeach
-                        </tbody>
-                    </table>
+                    <div class="table-responsive-sm">
+                        <table class="table table-bordered">
+                            <thead>
+                              <tr>
+                                <th>NISN</th>
+                                <th>Nama</th>
+                                <th>Tgl Lahir</th>
+                                <th>Gender</th>
+                                <th><a href="siswa/create" class="btn btn-primary btn-sm">Tambah</a></th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              @foreach ($siswa_list as $siswa)
+                                <tr>
+                                    <td>{{ $siswa->nisn }}</td>
+                                    <td>{{ $siswa->nama_siswa }}</td>
+                                    <td>{{ $siswa->tanggal_lahir->format('d-m-Y') }}</td>
+                                    <td>{{ $siswa->jenis_kelamin }}</td>
+                                    <td>
+                                        <div class="box-button">
+                                            {{ link_to('siswa/'.$siswa->id,'Detail',['class'=>'btn btn-info btn-sm']) }}
+                                        </div>
+                                        <div class="box-button">
+                                            {{ link_to('siswa/'.$siswa->id.'/edit','Edit',['class'=>'btn btn-warning btn-sm']) }}
+                                        </div>
+                                        <div class="box-button">
+                                            {!! Form::open(['method'=> 'Delete','action'=>['SiswaController@destroy',$siswa->id]]) !!}
+                                            {!! Form::submit('Delete',['class'=> 'btn btn-danger btn-sm']) !!}
+                                            {!! Form::close() !!}
+                                        </div>
+                                    </td>
+                                </tr>
+                              @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 @else
                  <p>Tidak ada data siswa</p>
                 @endif
