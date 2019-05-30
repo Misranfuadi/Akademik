cc187eed2462cddc2a5bd6a5eef41cdd3cab2462
Misranfuadi <misranfalcon@gmail.com>
2019-05-30 14:47:58 +0700

    fix error mssg and error validation

--- resources/views/pages/siswa/create.blade.php
+++ resources/views/pages/siswa/create.blade.php
@@ -5,14 +5,7 @@
     <div class="card col-md-8" id="siswa">
         <h4 class="card-header bg-dark text-light mb-3">Tambah Siswa</h4>
         <div class="card-body">
-                @if ($errors->any())
-                <ul class="alert alert-danger">
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            @endif
-            {!! Form::open (['url'=> 'siswa']) !!}
+            {!! Form::open (['url'=> 'siswa', 'class'=>'form-horizontal']) !!}
                 @include('pages.siswa.form',['submitButtonText'=>'Tambah Siswa'])
             {!! Form::close() !!}
         </div>
--- resources/views/pages/siswa/form.blade.php
+++ resources/views/pages/siswa/form.blade.php
@@ -1,14 +1,27 @@
-<div class="form-group">
+ @if (isset($siswa))
+    {!! Form::hidden('id',$siswa->id) !!}
+ @endif
+
+ <div class="form-group">
     {!! Form::label ('nisn','NISN:',['class'=>'control-label']) !!}
-    {!! Form::text ('nisn', null,['class'=>'form-control']) !!}
+    {!! Form::text ('nisn', null,['class'=>'form-control ']) !!}
+    @error('nisn')
+    <div class="bg-danger text-light">{{ $message }}</div>
+    @enderror
 </div>
 <div class="form-group">
     {!! Form::label ('nama_siswa','Nama:',['class'=>'control-label']) !!}
     {!! Form::text ('nama_siswa',null,['class'=>'form-control']) !!}
+    @error('nama_siswa')
+    <div class="bg-danger text-light">{{ $message }}</div>
+    @enderror
 </div>
 <div class="form-group">
     {!! Form::label ('tanggal_lahir','Tanggal Lahir:',['class'=>'control-label']) !!}
     {!! Form::date ('tanggal_lahir', !empty($siswa)?$siswa->tanggal_lahir->format('Y-m-d'):null,['class'=>'form-control']) !!}
+    @error('tanggal_lahir')
+    <div class="bg-danger text-light">{{ $message }}</div>
+    @enderror
 </div>
 <div class="form-group">
     {!! Form::label ('jenis_kelamin','Jenis Kelamin:',['class'=>'control-label']) !!}
@@ -18,6 +31,9 @@
     <div class="radio">
         <label>{!! Form::radio ('jenis_kelamin','P') !!}Perempuan</label>
     </div>
+    @error('jenis_kelamin')
+    <div class="bg-danger text-light">{{ $message }}</div>
+    @enderror
 </div>
 <div class="form-group">
     {!! Form::submit($submitButtonText,['class'=>'btn btn-primary form-control']) !!}
